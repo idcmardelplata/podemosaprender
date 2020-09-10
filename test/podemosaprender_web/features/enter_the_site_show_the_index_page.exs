0c5d51b7c6e26fe649669a7d8a192722aff5ccb7
Martin Algañaraz <idcmardelplata@gmail.com>
2020-09-10 17:45:09 -0300

    Feature Test: Register Page
    Agregado un test para que cuando el usuario
    ingrese al sitio lo primero que vea debe ser
    la pagina de registro.

--- test/podemosaprender_web/features/enter_the_site_show_the_index_page.exs
+++ test/podemosaprender_web/features/enter_the_site_show_the_index_page.exs
@@ -4,6 +4,8 @@ defmodule PodemosAprenderWeb.EnterTheSiteShowTheIndexPage do
   test "Users can visit the homepage", %{session: session} do
     session
     |> visit("/")
-    |> assert_has(Query.css("h1", text: "Welcome to Phoenix!"))
+    |> assert_has(Query.css("h1", text: "Registrarme"))
   end
+
+
 end
